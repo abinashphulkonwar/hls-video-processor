729b9036eb9ea054cc03b7253344b996f0c4f26d
abinash <abinashphulkonwar1@gmail.com>
2022-11-27 19:38:17 +0530

    add 240, 360 and 720 p

--- services/video-worker.js
+++ services/video-worker.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = __importDefault(require("path"));
@@ -9,52 +11,69 @@ const fluent_ffmpeg_1 = __importDefault(require("fluent-ffmpeg"));
 const crypto_1 = require("crypto");
 const fs_2 = require("fs");
 const process = (data, quality = "854x480", fileName = "") => {
-    return new Promise((resolve, reject) => {
-        const folderPath = path_1.default.join(__dirname, "..", "output", fileName, quality);
-        if (!(0, fs_2.existsSync)(fileName)) {
-            (0, fs_1.mkdirSync)(folderPath);
-        }
-        (0, fluent_ffmpeg_1.default)(path_1.default.join(__dirname, "..", "./inputs/input.mp4"))
-            .outputOptions([
-            "-profile:v baseline",
-            "-level 3.0",
-            `-s ${quality}`,
-            "-r 30",
-            "-start_number 0",
-            "-hls_time 10",
-            "-hls_list_size 0",
-            "-f hls",
-        ])
-            .output(`output/${fileName}/${fileName}.m3u8`)
-            .on("progress", function (progress) {
-            console.log("Processing: " + progress.percent + "% done");
-        })
-            .on("end", function (err, stdout, stderr) {
-            const fileNames = (0, fs_1.readdirSync)(folderPath);
-            return resolve({ fileName, fileNames, folderPath });
-        })
-            .on("error", (err) => {
-            return reject(err);
-        })
-            .run();
-    });
+  return new Promise((resolve, reject) => {
+    const folderPath = path_1.default.join(
+      __dirname,
+      "..",
+      "output",
+      fileName,
+      quality
+    );
+    if (!(0, fs_2.existsSync)(fileName)) {
+      (0, fs_1.mkdirSync)(folderPath);
+    }
+    (0, fluent_ffmpeg_1.default)(
+      path_1.default.join(__dirname, "..", "./inputs/input.mp4")
+    )
+      .outputOptions([
+        "-profile:v baseline",
+        "-level 3.0",
+        `-s ${quality}`,
+        "-r 30",
+        "-start_number 0",
+        "-hls_time 10",
+        "-hls_list_size 0",
+        "-f hls",
+      ])
+      .output(`output/${fileName}/${fileName}.m3u8`)
+      .on("progress", function (progress) {
+        console.log("Processing: " + progress.percent + "% done");
+      })
+      .on("end", function (err, stdout, stderr) {
+        const fileNames = (0, fs_1.readdirSync)(folderPath);
+        return resolve({ fileName, fileNames, folderPath });
+      })
+      .on("error", (err) => {
+        return reject(err);
+      })
+      .run();
+  });
 };
 const workerHandler = async (job) => {
-    // const data = readFileSync(path.join(__dirname, "..", "./inputs/input.mp4"));
-    console.time("start");
-    const fileName = (0, crypto_1.randomUUID)();
-    const folderPath = path_1.default.join(__dirname, "..", "output", fileName);
-    if (!(0, fs_2.existsSync)(fileName)) {
-        (0, fs_1.mkdirSync)(folderPath);
-    }
-    const process640x360 = await process(job === null || job === void 0 ? void 0 : job.data, "640x360", fileName);
-    const process1280x720 = await process(job === null || job === void 0 ? void 0 : job.data, "1280x720", fileName);
-    // await uploadVideoTos3({
-    //   event: "hls",
-    //   job: job.data,
-    //   folderPath,
-    // });
-    console.timeEnd("start");
+  // const data = readFileSync(path.join(__dirname, "..", "./inputs/input.mp4"));
+  console.time("start");
+  const fileName = (0, crypto_1.randomUUID)();
+  const folderPath = path_1.default.join(__dirname, "..", "output", fileName);
+  if (!(0, fs_2.existsSync)(fileName)) {
+    (0, fs_1.mkdirSync)(folderPath);
+  }
+  const process640x360 = await process(
+    job === null || job === void 0 ? void 0 : job.data,
+    "640x360",
+    fileName
+  );
+  const process1280x720 = await process(
+    job === null || job === void 0 ? void 0 : job.data,
+    "1280x720",
+    fileName
+  );
+
+  // await uploadVideoTos3({
+  //   event: "hls",
+  //   job: job.data,
+  //   folderPath,
+  // });
+  console.timeEnd("start");
 };
 workerHandler(null);
 exports.default = workerHandler;
--- services/video-worker.ts
+++ services/video-worker.ts
@@ -13,14 +13,22 @@ interface fileInterface {
 }
 
 enum qualityEnum {
+  "480x240" = "480x240",
   "1280x720" = "1280x720",
   "640x360" = "640x360",
 }
 enum qualityFileName {
   "1280x720" = "720_index",
   "640x360" = "360_index",
+  "480x240" = "240_index",
 }
-const process = (data: any, quality: qualityEnum, fileName = "") => {
+const process = (
+  data: any,
+  quality: qualityEnum,
+  fileName = "",
+  bitrate = "8M",
+  duration = 10
+) => {
   return new Promise<fileInterface>((resolve, reject) => {
     const folderPath = path.join(__dirname, "..", "output", fileName, quality);
     if (!existsSync(fileName)) {
@@ -31,16 +39,18 @@ const process = (data: any, quality: qualityEnum, fileName = "") => {
       indexFile = qualityFileName["640x360"];
     if (quality == qualityEnum["1280x720"])
       indexFile = qualityFileName["1280x720"];
-
-    Ffmpeg(path.join(__dirname, "..", "./inputs/input.mp4"))
+    // 20221002_203427
+    Ffmpeg(path.join(__dirname, "..", "./inputs/out.mp4"))
       .outputOptions([
         "-profile:v baseline",
         "-level 3.0",
         `-s ${quality}`,
         "-r 30",
+        `-b:v ${bitrate}`,
         "-start_number 0",
-        "-hls_time 10",
+        `-hls_time ${duration}`,
         "-hls_list_size 0",
+        "-filter:v fps=30",
         "-f hls",
       ])
       .output(`output/${fileName}/${quality}/${indexFile}.m3u8`)
@@ -69,30 +79,50 @@ const workerHandler = async (job: SandboxedJob | null) => {
   const process640x360 = await process(
     job?.data,
     qualityEnum["640x360"],
-    fileName
+    fileName,
+    "1.5M",
+    5
   );
+  const process480x240 = await process(
+    job?.data,
+    qualityEnum["480x240"],
+    fileName,
+    "1M",
+    5
+  );
+
   const process1280x720 = await process(
     job?.data,
     qualityEnum["1280x720"],
-    fileName
+    fileName,
+    "5M",
+    10
   );
 
   writeFileSync(
     folderPath + "/index.m3u8",
     `#EXTM3U
-#EXT-X-STREAM-INF:BANDWIDTH=258157,RESOLUTION=640x360
+#EXT-X-STREAM-INF:BANDWIDTH=258157,RESOLUTION=480x240
+480x240/240_index.m3u8
+#EXT-X-STREAM-INF:BANDWIDTH=858157,RESOLUTION=640x360
 640x360/360_index.m3u8
 #EXT-X-STREAM-INF:BANDWIDTH=2000000,RESOLUTION=1280x720
 1280x720/720_index.m3u8`
   );
-
-  // await uploadVideoTos3({
-  //   event: "hls",
-  //   job: job.data,
-  //   folderPath,
-  // });
+  return;
+  await uploadVideoTos3({
+    event: "hls",
+    job: job?.data,
+    folderPath: process640x360,
+  });
+  await uploadVideoTos3({
+    event: "hls",
+    job: job?.data,
+    folderPath: process1280x720,
+  });
   console.timeEnd("start");
 };
+
 workerHandler(null);
 
 export default workerHandler;
